959445b1b9a62b2dea97a3e28e24b83494679bed
daimingyu <daimingyu@58ganji.com>
2019-08-20 16:27:59 +0800

    跨域-cors

---
+++ 跨域/CORS/server/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+// 创建服务器
+const app = express()
+
+// 当服务区 收到get 请求 '/' 时， 指向回调处理函数
+app.get('/', function (req, res) {
+  res.send('this is server')
+})
+
+app.get('/data', function (req, res) {
+  console.log(req.query.callback)
+  var result = {
+    "name": "tom",
+    "age": 13
+  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send(JSON.stringify(result));
+})
+
+// 在 express 中 开放资源 就是一个 api 的事
+app.use('/static/', express.static('./static/'))
+// http://localhost:3000/static/loading.gif 即可访问图片
+
+// 建立监听端口 相当于 server.listen
+app.listen(3000, function () {
+  console.log('app is running at http://localhost:3000')
+})
